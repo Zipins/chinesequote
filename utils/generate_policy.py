4d4bd2639904a85f2933dedf2167e9aca2418bc3
Zipins <sam@zipins.net>
2025-07-31 21:29:11 -0500

    Update generate_policy.py

--- utils/generate_policy.py
+++ utils/generate_policy.py
@@ -6,15 +6,48 @@ def generate_policy_docx(doc, data):
     replace_placeholder_text(doc, "XXXXXXXXXXX", data.get("company", "某保险公司"))
     replace_placeholder_text(doc, "$XXXXXX/X个月", f"{data.get('total_premium', '$XXX')}/{data.get('policy_term', '6个月')}")
 
+    # 责任险 Liability
     write_checkbox_and_amount(doc, "Liability", data["liability"]["selected"])
     if data["liability"]["selected"]:
-        replace_text_in_paragraphs(doc, "赔偿对方医疗费最高$XXXX/人", f"赔偿对方医疗费最高{data['liability']['bi_per_person']}/人")
-        replace_text_in_paragraphs(doc, "赔偿对方医疗费总额最高$XXX", f"赔偿对方医疗费总额最高{data['liability']['bi_per_accident']}")
-        replace_text_in_paragraphs(doc, "赔偿对方车辆和财产损失最多$XXXX", f"赔偿对方车辆和财产损失最多{data['liability']['pd']}")
+        replace_in_all_paragraphs(doc, "$XXXX/人", data['liability']['bi_per_person'])
+        replace_in_all_paragraphs(doc, "$XXX", data['liability']['bi_per_accident'], occurrence=1)
+        replace_in_all_paragraphs(doc, "$XXXX", data['liability']['pd'], occurrence=1)
     else:
         replace_text_in_paragraphs(doc, "赔偿对方医疗费最高$XXXX/人", "没有选择该项目")
         replace_text_in_paragraphs(doc, "赔偿对方医疗费总额最高$XXX", "")
         replace_text_in_paragraphs(doc, "赔偿对方车辆和财产损失最多$XXXX", "")
+
+    # 无保险驾驶者 Uninsured Motorist
+    write_checkbox_and_amount(doc, "Uninsured Motorist", data["uninsured_motorist"]["selected"])
+    if data["uninsured_motorist"]["selected"]:
+        replace_text_in_paragraphs(doc, "赔偿你和乘客医疗费$XXXX/人", f"赔偿你和乘客医疗费{data['uninsured_motorist']['bi_per_person']}/人")
+        replace_text_in_paragraphs(doc, "一场事故最多赔偿医疗费$XXXX", f"一场事故最多赔偿医疗费{data['uninsured_motorist']['bi_per_accident']}")
+        replace_text_in_paragraphs(doc, "赔偿自己车辆最多$XXX(自付额$250)", f"赔偿自己车辆最多{data['uninsured_motorist']['pd']}(自付额$250)")
+    else:
+        replace_text_in_paragraphs(doc, "赔偿你和乘客医疗费$XXXX/人", "没有选择该项目")
+        replace_text_in_paragraphs(doc, "一场事故最多赔偿医疗费$XXXX", "")
+        replace_text_in_paragraphs(doc, "赔偿自己车辆最多$XXX(自付额$250)", "")
+
+    # 医疗费用 Medical Payment
+    write_checkbox_and_amount(doc, "Medical Payment", data["medical_payment"]["selected"])
+    if data["medical_payment"]["selected"]:
+        replace_text_in_paragraphs(doc, "赔偿自己和自己车上乘客在事故中受伤的医疗费每人$XXX",
+                                    f"赔偿自己和自己车上乘客在事故中受伤的医疗费每人{data['medical_payment']['med']}")
+    else:
+        replace_text_in_paragraphs(doc, "赔偿自己和自己车上乘客在事故中受伤的医疗费每人$XXX", "没有选择该项目")
+
+    # 人身伤害 Personal Injury
+    write_checkbox_and_amount(doc, "Personal Injury", data["personal_injury"]["selected"])
+    if data["personal_injury"]["selected"]:
+        replace_text_in_paragraphs(doc,
+            "赔偿自己和自己车上乘客在事故中受伤的医疗费，误工费和精神损失费每人$XXX",
+            f"赔偿自己和自己车上乘客在事故中受伤的医疗费，误工费和精神损失费每人{data['personal_injury']['pip']}"
+        )
+    else:
+        replace_text_in_paragraphs(doc,
+            "赔偿自己和自己车上乘客在事故中受伤的医疗费，误工费和精神损失费每人$XXX",
+            "没有选择该项目"
+        )
 
 
 def replace_placeholder_text(doc, placeholder, replacement):
@@ -28,6 +61,16 @@ def replace_text_in_paragraphs(doc, old, new):
         for run in paragraph.runs:
             if old in run.text:
                 run.text = run.text.replace(old, new)
+
+
+def replace_in_all_paragraphs(doc, keyword, replacement, occurrence=None):
+    count = 0
+    for paragraph in doc.paragraphs:
+        for run in paragraph.runs:
+            if keyword in run.text:
+                if occurrence is None or count < occurrence:
+                    run.text = run.text.replace(keyword, replacement)
+                    count += 1
 
 
 def write_checkbox_and_amount(doc, keyword, selected):
