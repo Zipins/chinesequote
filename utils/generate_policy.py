4b7e87db6e39e746110d76922b4935beffda0333
Zipins <sam@zipins.net>
2025-07-31 21:14:23 -0500

    Update generate_policy.py

--- utils/generate_policy.py
+++ utils/generate_policy.py
@@ -2,7 +2,7 @@
 from docx import Document
 from docx.shared import Pt, RGBColor
 from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.oxml import OxmlElement
+from utils.generate_policy_fixed import generate_policy_docx
 from copy import deepcopy
 
 
