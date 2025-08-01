67fcb293905df7c6b7826c9b3a17136d602b5bfe
Zipins <sam@zipins.net>
2025-07-31 21:24:13 -0500

    Update generate_policy.py

--- utils/generate_policy.py
+++ utils/generate_policy.py
@@ -1,2 +1,9 @@
+
 def generate_policy_docx(doc, data):
-    print("generate_policy_docx called with", doc, data)
+    replace_placeholder_text(doc, "XXXXXXXXXXX", data.get("company", "某保险公司"))
+    replace_placeholder_text(doc, "$XXXXXX/X个月", f"{data.get('total_premium', '$XXX')}/{data.get('policy_term', '6个月')}")
+
+def replace_placeholder_text(doc, placeholder, replacement):
+    for paragraph in doc.paragraphs:
+        if placeholder in paragraph.text:
+            paragraph.text = paragraph.text.replace(placeholder, replacement)
